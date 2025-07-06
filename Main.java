b0a0f6b3ea102c1bad8f2b110e0efc7bac2c7db7
GITTY5678 <rge7311@gmail.com>
2025-07-06 10:05:32 +0530

    interactive cli completed

--- Main.java
+++ Main.java
@@ -1,7 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player("Alice", 1500, 10);
-        Player player2 = new Player("Bob", 2000, 15);   
-        player1.display();
-        player2.display();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Player> players = new ArrayList<>();
+
+        while (true){
+            System.out.println("1.Add player");
+            System.out.println("2.display players");
+            System.out.println("3.exit");
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline character
+
+            switch (choice){
+                case 1:
+                System.out.print("Enter player name: ");
+                String name = scanner.nextLine();
+                System.out.print("Enter total matchesplayed: ");
+                int matchesPlayed = scanner.nextInt();
+                System.out.print("Enter total runs: ");
+                int score = scanner.nextInt();
+                players.add(new Player(name, score, matchesPlayed));
+                break;
+
+                case 2:
+                if (players.isEmpty()) {
+                    System.out.println("No players to display.");
+                } else {
+                    for (Player player : players) {
+                        player.display();
+                    }
+                }
+                break;
+                case 3:
+                System.out.println("Exiting the program.");
+                break;
+
+                default:
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
 }}
